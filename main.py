6d03bf474e2b2cec08c936fc32ba6eba723fd59f
john <jdpennington0@gmail.com>
2025-10-02 12:51:12 -0700

    Remove 404 admin alerts.

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ def page_not_found(e):
     # If 'abc' is included, beetlebox.dev/abc/xyz is redirected to abc.beetlebox.dev/xyz
 
     skip_endpoints = tuple()  # Skip any endpoints requiring request.args, and any others to not suggest for redirect.
-    ignore_paths_starting_with = [  # Doesn't send an admin alert if request.path starts with any of these.
-        '20', 'admin', 'blog', 'cms', 'feed', 'media', 'misc', 'news', 'robots', 'site', 'sito',
-        'shop', 'test', 'web', 'wordpress', 'Wordpress', 'wp', 'Wp', 'xmlrpc.php',
-    ]
+    # ignore_paths_starting_with = [  # Doesn't send an admin alert if request.path starts with any of these.
+    #     '20', 'admin', 'blog', 'cms', 'feed', 'media', 'misc', 'news', 'robots', 'site', 'sito',
+    #     'shop', 'test', 'web', 'wordpress', 'Wordpress', 'wp', 'Wp', 'xmlrpc.php',
+    # ]
 
     # Redirect old url to new subdomain. i.e. beetlebox.dev/core/xyz => core.beetlebox.dev/xyz
     for subdomain_name in subdomain_redirects:
@@ -39,31 +39,16 @@ def page_not_found(e):
     a_text = site_root
     a_href = '/'
 
-    request_of_concern = True  # Requests of concern are paths not filtered by ignore_paths_starting_with.
-    # If request_of_concern remains True, an attempt is made to suggest a more specific url on not found page,
-    # and an admin alert is sent.
-    for path_to_ignore in ignore_paths_starting_with:
-        if request.path.startswith(f'/{path_to_ignore}'):
-            print('Not a request of concern.')
-            request_of_concern = False
-            break
-
-    if request_of_concern:
-
-        # Look for nearest valid path.
-        for rule in app.url_map.iter_rules():
-            if "GET" in rule.methods and rule.endpoint not in skip_endpoints and len(rule.arguments) == 0:
-                # Static folder has rule.arguments, so is skipped and rerouted to root.
-                if request.path.startswith(rule.rule):  # Rule.rule is relative path.
-                    a_href = url_for(rule.endpoint)
-                    if a_href == '/':
-                        continue  # Otherwise, displays final slash after site root <a> text.
-                    a_text = f'{site_root}<wbr>{a_href}'
-                    break
-
-        message_body = f'Page not found: \n{request.url}\n' \
-                       f'Rendered page_not_found.html and suggested: \n{site_root}{a_href}'
-        admin_alert_thread('Web App - 404', message_body)
+    # Look for nearest valid path.
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and rule.endpoint not in skip_endpoints and len(rule.arguments) == 0:
+            # Static folder has rule.arguments, so is skipped and rerouted to root.
+            if request.path.startswith(rule.rule):  # Rule.rule is relative path.
+                a_href = url_for(rule.endpoint)
+                if a_href == '/':
+                    continue  # Otherwise, displays final slash after site root <a> text.
+                a_text = f'{site_root}<wbr>{a_href}'
+                break
 
     return render_template('page_not_found.html', relpath=a_href, a_text=a_text, copyright_notice=copyright_notice), 404
 
